121c2314d4bc4f1936935314e5fc71bc9794e30e
sandesh magdum <sandesh.s_magdum@yahoo.co.in>
2019-07-30 17:34:41 +0530

    Added Sign Up with email and password feature

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth, createProfileUserDocument } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -19,14 +19,19 @@ class App extends Component {
   componentDidMount() {
     this.unsubcribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
-        const userRef = await createProfileUserDocument(userAuth);
-        userRef.onSnapshot(snapshot => {
-          this.setState({
-            currentUser: {
-              id: snapshot.id,
-              ...snapshot.data()
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            },
+            () => {
+              console.log(this.state);
             }
-          });
+          );
         });
       }
       this.setState({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,7 @@ const config = {
   appId: "1:439953958049:web:3cec380fcac25c7c"
 };
 
-export const createProfileUserDocument = async (userAuth, additionalData) => {
+export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
   const { displayName, email, uid } = userAuth;
   const userRef = firestore.doc(`users/${uid}`);
